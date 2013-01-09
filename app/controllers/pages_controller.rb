25181cd0049c84613369e3c1c133c609dc0b92f8
Justus Ohlhaver <ohlhaver@gmail.com>
2013-01-09 21:32:58 +0100

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+	def home
+	end
+
+	def de
+	end
+
+	def set_locale
+		session[:locale] = params[:locale]
+		I18n.locale = session[:locale]
+		redirect_to ( request.referer || root_path ).gsub(/locale=\w+(\&|$)/, '')
+	end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accelerately::Application.config.secret_token = 'c2777e6955bb06917883987b9fbc954996a2ad964d391f3ddb4aaf8acda7d32868c9a113057fafe2e6752872e9142af07a6f2cc1e9e242f4217a7c9511a322fb'
