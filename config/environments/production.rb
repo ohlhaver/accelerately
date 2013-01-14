d4c6f97ef381dfa998399a9709456c766368c851
Justus Ohlhaver <ohlhaver@gmail.com>
2013-01-14 16:22:53 +0100

    added config.action_dispatch.rack_cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,11 @@ Accelerately::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+    config.action_dispatch.rack_cache = {
+                          :metastore    => Dalli::Client.new,
+                          :entitystore  => 'file:tmp/cache/rack/body',
+                          :allow_reload => false } # very changed
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
